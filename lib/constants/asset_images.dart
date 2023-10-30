e88c62df41878c5a81394813fa2dd2bc42cdf47c
mwaidesu <machariamwai@gmail.com>
2023-10-30 17:19:40 +0300

    Add cupertino button to site logos

--- lib/constants/asset_images.dart
+++ lib/constants/asset_images.dart
@@ -1,7 +1,7 @@
 class AssetsImages {
   static AssetsImages instance = AssetsImages();
 
-  static const String _imagesPath = "assets/images/";
+  static const String _imagesPath = "assets/images";
   String welcomeImage = "$_imagesPath/boom.png";
   String instagramImage = "$_imagesPath/instagram.png";
   String googleImage = "$_imagesPath/google.png";
--- lib/screens/auth_ui/login/login.dart
+++ lib/screens/auth_ui/login/login.dart
@@ -1,53 +1,66 @@
 import 'package:comicsduka/constants/asset_images.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
-    @override
+  @override
   Widget build(BuildContext context) {
-    return  SafeArea(child: Scaffold(
-        body: Padding(
+    return SafeArea(
+        child: Scaffold(
+            body: Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const Text("Welcome", style:TextStyle(
-            fontSize: 18.0,
-            fontWeight: FontWeight.bold,
-          ),),
+          const Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           const SizedBox(
             // height: kToolbarHeight +12,
             height: 12,
           ),
-          const Text("Get your favorite comics", style: TextStyle(
-            fontSize: 18.0,
-          ),),
-          Center(child: Image.asset(AssetsImages.instance.welcomeImage,
+          const Text(
+            "Get your favorite comics",
+            style: TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
+          Center(
+              child: Image.asset(
+            AssetsImages.instance.welcomeImage,
           )),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Icon(Icons.facebook,
-              size: 35,
-              color: Colors.blue,
-               ),
-                         // Image.asset(
-          //   AssetsImages.instance.googleImage,
-          //   scale: 30.0,
-          // ),
-           Image.asset(
-            AssetsImages.instance.instagramImage,
-            scale: 30.0,
-          ),
+              CupertinoButton(
+                onPressed:(){},
+                padding: EdgeInsets.zero,
+                child: Image.asset(
+                  AssetsImages.instance.googleImage,
+                  scale: 30.0,
+                ),
+              ),
+              const SizedBox(
+                width: 12.0,
+              ),
+              CupertinoButton(
+                onPressed:(){},
+                padding: EdgeInsets.zero,
+                child: Image.asset(
+                  AssetsImages.instance.instagramImage,
+                  scale: 30.0,
+                ),
+              ),
             ],
           ),
-
-
         ],
-
       ),
     )));
-  
   }
 }
