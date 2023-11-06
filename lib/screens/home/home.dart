d7b63b63f07a1c980e39313eb43a1a90a379b795
mwaidesu <machariamwai@gmail.com>
2023-11-06 17:30:22 +0300

    Remove titles from categories

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -38,68 +38,38 @@ class Home extends StatelessWidget {
             ),
           ),
 
-          // SingleChildScrollView(
-          //   scrollDirection: Axis.horizontal,
-          //   child: Row(children: 
-          //     categoriesList.map((e) => 
-          //     Padding(
-          //     padding: const EdgeInsets.only(left: 8.0),
-          //     child: Card(
-          //       color: Colors.white,
-          //       elevation: 13.0,
-          //       shape: RoundedRectangleBorder(
-          //           borderRadius: BorderRadius.circular(20.0)),
-          //       child: Container(
-          //         height: 100,
-          //         width: 100,
-          //         child: Image.network(e),
-          //       ),
-          //     ),
-          //   ),
-          //   ).toList(),
-          //   ),
-          // ),
-
-
           SingleChildScrollView(
-  scrollDirection: Axis.horizontal,
-  child: Row(
-    children: categoriesList.asMap().entries.map((entry) {
-      final int index = entry.key;
-      final String imageUrl = entry.value;
-
-      return Padding(
-        padding: const EdgeInsets.only(left: 8.0),
-        child: Card(
-          color: Colors.white,
-          elevation: 3.0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: categoriesList
+                  .map(
+                    (e) => Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Card(
+                        color: Colors.white,
+                        elevation: 3.0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)),
+                        child: Container(
+                          height: 100,
+                          width: 100,
+                          child: Image.network(e),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
           ),
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 130,
-                width: 150,
-                child: Image.network(imageUrl),
-              ),
-              // SizedBox(height: 8.0), // Add spacing between the image and text
-              Padding(
-                padding: const EdgeInsets.only(bottom: 12.0),
-                child: Text(
-                  titles[index],
-                  style: TextStyle(fontSize: 16.0),
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-    }).toList(),
-  ),
-)
 
-          
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: const Text(
+              "Best Comics",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+
         ],
       ),
     );
@@ -107,16 +77,11 @@ class Home extends StatelessWidget {
 }
 
 List<String> categoriesList = [
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfdAZFZGOIxL3eEEjkiorv8lH5Our3D-9yASfMp4wppA&s",
-  "https://www.dlf.pt/dfpng/middlepng/1-17857_captain-america-shield-png-transparent-png.png",
-  "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f05a9b5b-ead5-460e-8573-73ba2fff9cde/dcnlwkp-19df6a40-badc-4fe9-803d-9da05f5c68bc.png/v1/fill/w_1600,h_1237/akatsuki_cloud_png_by_chanxgg_dcnlwkp-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTIzNyIsInBhdGgiOiJcL2ZcL2YwNWE5YjViLWVhZDUtNDYwZS04NTczLTczYmEyZmZmOWNkZVwvZGNubHdrcC0xOWRmNmE0MC1iYWRjLTRmZTktODAzZC05ZGEwNWY1YzY4YmMucG5nIiwid2lkdGgiOiI8PTE2MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.boOIlfNZMoKSfgoa3iXmZEM4bT79B9TTEb_qZb-3h6M",
-
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/768px-DC_Comics_logo.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/1280px-Marvel_Logo.svg.png",
+  "https://seeklogo.com/images/M/manga-logo-26D5521A34-seeklogo.com.png",
   "https://upload.wikimedia.org/wikipedia/commons/0/09/Naver_Line_Webtoon_logo.png",
-
   "https://indiecomicszone.com/wp-content/uploads/2022/06/logo-black-220.png"
-
-
-
 ];
 
 List<String> titles = [
